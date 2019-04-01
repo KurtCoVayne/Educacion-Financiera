69554fc535837068c730fef8730610fd5c0dac01
John Alejandro Gonzalez <johnalejandrog.g4@gmail.com>
2019-04-01 08:07:36 -0500

    Added some other things

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.get('/problematica', (req, res ) =>{
+  res.render('problematica');
+});
+router.get('/solucion', (req, res) => {
+  res.render('solucion');
+});
+router.get('/ejemplos', (req, res) => {
+  res.render('solucion');
+});
+router.get('/conceptos', (req, res) => {
+  res.render('solucion');
 });
 
 module.exports = router;
